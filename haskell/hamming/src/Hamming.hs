3ab3339e5341f7ee97895ce2c94c609365ec04a2
pulsation <philippe.sam-long@laposte.net>
2018-03-03 22:23:05 +0100

    simplify

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -3,9 +3,4 @@ module Hamming (distance) where
 distance :: String -> String -> Maybe Int
 distance xs ys
   | length xs /= length ys = Nothing
-  | otherwise = Just $ sum $ zipWith mutation xs ys
-
-mutation :: Char -> Char -> Int
-mutation x y
-  | x /= y = 1
-  | otherwise = 0
+  | otherwise = Just $ length $ filter id $ zipWith (/=) xs ys
